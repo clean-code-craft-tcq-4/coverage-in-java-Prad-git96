9cea55edefbcc9ede1555832ef229f73985b1234
Patange Pradnya Prasad (SX/EMS2) <ptp2kor@bosch.com>
2022-07-22 06:31:58 +0530

    alerter refactoring

--- src/main/java/typewisealert/enums/BreachType.java
+++ src/main/java/typewisealert/enums/BreachType.java
@@ -2,9 +2,17 @@ package typewisealert.enums;
 
 
 public enum BreachType {
-  NORMAL,
-  TOO_LOW,
-  TOO_HIGH;
+  NORMAL("normal"),
+  TOO_LOW("too low"),
+  TOO_HIGH("too high");
   
-  
+	String value;
+	
+	BreachType(String breachType){
+		this.value=breachType;
+	}
+	
+	public String  getBreachType() {
+		return this.value;
+	}
 }
--- src/main/java/typewisealert/notification/EmailNotificationSender.java
+++ src/main/java/typewisealert/notification/EmailNotificationSender.java
@@ -7,18 +7,12 @@ public class EmailNotificationSender implements Notifier{
 	@Override
 	public void sendNotification(BreachType breachType) {
 		 String recepient = "a.b@c.com";
-	      switch(breachType) {
-	        case TOO_LOW:
-	          System.out.printf("To: %s\n", recepient);
-	          System.out.println("Hi, the temperature is too low\n");
-	          break;
-	        case TOO_HIGH:
-	          System.out.printf("To: %s\n", recepient);
-	          System.out.println("Hi, the temperature is too high\n");
-	          break;
-	        case NORMAL:
-	          break;
-	      }		
+     	sendEmail(recepient, breachType.getBreachType());
+	}
+	
+	public void sendEmail(String recepient,String breachType) {
+        System.out.printf("To: %s\n", recepient);
+        System.out.println("Hi, the temperature is "+breachType+ "\n");
 	}
 
 }
